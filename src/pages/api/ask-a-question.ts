0340da7d0dabde3cbd4c5c8497d47deea1473c6f
José Paulo <jose.silva@bandtec.com.br>
2023-02-18 18:52:37 -0300

    refactor: adicionando timeout

--- src/pages/api/ask-a-question.ts
+++ src/pages/api/ask-a-question.ts
@@ -6,20 +6,37 @@ type Data = {
     error?: string
 }
 
+const timeoutPromise = (timeout: number) =>
+    new Promise<never>((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error('Tempo limite excedido'));
+        }, timeout);
+    });
+
+const createCompletionWithTimeout = async (
+    question: string,
+    timeout: number
+) => {
+    const completion = openAI.createCompletion({
+        model: 'text-davinci-002',
+        prompt: question,
+        temperature: 0.8,
+        max_tokens: 2048
+    });
+
+    return Promise.race([completion, timeoutPromise(timeout)]);
+};
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
     if (!req.body?.question) {
-        res.status(400).json({error: 'Invalid params'})
+        res.status(400).json({ error: 'Invalid params' })
     }
 
-    const completion = await openAI.createCompletion({
-        model: 'text-davinci-002',
-        prompt: req?.body?.question,
-        temperature: 0.8,
-        max_tokens: 2048
-    });
 
-    res.status(200).json({result: completion.data?.choices?.[0].text})
+    createCompletionWithTimeout(req?.body?.question, 120000).then((response) => {
+        return res.status(200).json({ result: response.data?.choices?.[0].text })
+    })
 }
